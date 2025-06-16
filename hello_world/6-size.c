cf60929cc2237d1a9274b1c9c889855197530f97
root <greg.sala@icloud.com>
2025-06-16 12:22:05 +0000

    Size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("\n Size of a char: %d byte(s)",sizeof(char));
-printf("\n Size of an int: %d byte(s)",sizeof(int));
-printf("\n Size of a long int: %d byte(s)",sizeof(long int));
-printf("\n Size of a long long int: %d byte(s)",sizeof(long long int));
-printf("\n Size of a float: %d byte(s)",sizeof(float));
+printf("\n Size of a char: %d byte(s)", sizeof(char));
+printf("\n Size of an int: %d byte(s)", sizeof(int));
+printf("\n Size of a long int: %d byte(s)", sizeof(long int));
+printf("\n Size of a long long int: %d byte(s)", sizeof(long long int));
+printf("\n Size of a float: %d byte(s)", sizeof(float));
 return (0);
 }
