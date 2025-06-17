8ec366b3ff88cdbd86227365c9899ce09d287958
root <greg.sala@icloud.com>
2025-06-17 12:04:25 +0000

    Add line exceed 80 characters

--- script_emacs_betty/script.sh
+++ script_emacs_betty/script.sh
@@ -3,3 +3,7 @@ echo "(setq c-default-style \"bsd\"
      c-basic-offset 8
      tab-width 8
      indent-tabs-mode t)" > /root/.emacs
+
+echo "(require 'whitespace)
+     (setq whitespace-style '(face empty lines-tail trailing))
+     (global-whitespace-mode t)" >> /root/.emacs
