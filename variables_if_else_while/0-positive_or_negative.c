d82667022965c27c67173c2164496aeb95275369
root <greg.sala@icloud.com>
2025-06-17 09:11:48 +0000

    Positive anything is better than negative nothing

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -6,24 +6,24 @@
 /* Affecter un nombre aléatoire a la variable n */
 int main(void)
 {
-	int n = 0;
-	if (n > 0)
-	{
-	printf("%d is positive\n", n);
-	}
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* affichage nombre aléatoire */
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
 
-	else if (n == 0)
-	{
-	printf("%d is zero\n", n);
-	}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
 
-	else
-	{
-	printf("%d is negative\n", n);
-	}
+else
+{
+printf("%d is negative\n", n);
+}
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* affichage nombre aléatoire */
-	return (0);
+return (0);
 }
