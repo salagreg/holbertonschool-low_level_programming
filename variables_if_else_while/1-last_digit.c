71dad0c3c2b5fe9da9520db3414e77eb0b4f1b58
root <greg.sala@icloud.com>
2025-06-17 12:37:55 +0000

    Last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -24,14 +24,10 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	}
-	else if (last_digit < 6 && last_digit > 0)
+	else
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n",
 		       n, last_digit);
-	}
-	else
-	{
-		printf("La valeur de %d est négative\n", last_digit);
 	}
 	return (0);
 }
