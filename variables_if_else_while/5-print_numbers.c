8afacfde738ed1f3b7ed873fca58c1dbd2bae7f4
root <greg.sala@icloud.com>
2025-06-18 11:12:44 +0000

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - fonction principale
+* c : neant
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+	
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
