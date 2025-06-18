3be5d1bedcc6a7358041c67a66f7b6aa525764d2
root <greg.sala@icloud.com>
2025-06-18 10:44:46 +0000

    Task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - fonction principale
+* c : neant
+* Return: 0
+*/
+int main(void)
+{
+	char c;
+	
+	for (c = 'a' ; c <= 'z' ; c++)
+		if (c != 'e')
+			if (c != 'q')
+			{
+				putchar(c);
+			}
+	putchar('\n');
+	return (0);
+}
