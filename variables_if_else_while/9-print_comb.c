9fb472a8e6621fae807adcd69c87f2c11db03620
root <greg.sala@icloud.com>
2025-06-18 14:55:45 +0000

    Task 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,12 +10,15 @@ int main(void)
 {
 	int i;
 
-	for (i = '0' ; i <= '8' ; i++)
+	for (i = '0' ; i <= '9' ; i++)
 	{
 		putchar(i);
-		putchar(',');
-		putchar(' ');
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar(i);
+	putchar('\n');
 	return (0);
 }
