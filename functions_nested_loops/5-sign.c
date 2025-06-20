3cdbd4fe3f4678b6f5d3f30175ef65acc0815876
root <greg.sala@icloud.com>
2025-06-20 10:02:08 +0000

    Sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_sign - raccourci fonction principale
+* @n: imprimer le signe d'un nombre
+* Return: 0 si 0, 1 si +, -1 si -
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+}
