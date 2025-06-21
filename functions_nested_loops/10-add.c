4413f73474efbe8857205bccde30719e85c014a3
root <greg.sala@icloud.com>
2025-06-21 16:28:00 +0000

    Addition entiers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* add - additionner deux valeurs
+* @i: valeur d'entier que je dois additionner
+* @j: valeur d'entier que je dois additionner
+* Return: n , je retourne le résultat de i + j
+*/
+int add(int i, int j) /* valeur que je dois additionner */
+{
+	int n = i + j;
+
+	return (n);
+}
