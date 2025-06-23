37a02ed9608bdc8d78eb95120e45f6fcdee1c97d
root <greg.sala@icloud.com>
2025-06-23 09:58:30 +0000

    Multiplie 2 nombres entiers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* mul - multiplie deux entiers
+* @a : nombre entier
+* @b : nombre entier
+* Return: a*b pour avoir la valeur de retour de la multiplication
+*/
+int mul(int a, int b)
+{
+	return (a*b);
+}
