c1ec543804a6a12a64f1f2c4afbd08a4f8bd4d30
root <greg.sala@icloud.com>
2025-06-24 22:00:34 +0000

    Afficher triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+* <nom de la fonction> - <explication de la fonction>
+* @<nom de l'argument>: <explication de l'argument> // Seulement s'il y a un ou plusieurs arguments
+* Return: <explication des valeurs de retour> // Seulement si la fonction retourne autre chose que void
+*/
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+	
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else if (size <= 2)
+	{
+		_putchar('#');
+		_putchar('\n');
+	}
+	else if (size > 2)
+	{
+		_putchar();
+	}
+	for (i = 1 ; i <= size ; i++)
+	{
+		for (j = 0 ; j < size -i ; j++)
+			_putchar(' ');
+
+		for (k = 0 ; k < i ; k++)
+			_putchar('#');
+		
+		_putchar('\n');
+	}
+}
