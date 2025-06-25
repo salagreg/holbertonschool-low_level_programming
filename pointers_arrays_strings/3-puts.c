9ae7e0c8125277c5c806e66fc03d15df06352e63
root <greg.sala@icloud.com>
2025-06-25 13:52:27 +0000

    imprimer chaine de caracteres

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _puts - imprime une chaine de caractères
+* @str : contient les valeurs de la chaine
+* Return: neant
+*/
+void _puts(char *str)
+{
+	puts(str);
+}
