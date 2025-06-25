13e52c789a1685b38bcab4a0663fc77431dd5522
root <greg.sala@icloud.com>
2025-06-25 12:07:17 +0000

    MAJ Pointeur

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* reset_to_98 - mettre à jour la valeur du pointeur
+* @n : contient les changements
+* Return: neant
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Pointeurs et string
