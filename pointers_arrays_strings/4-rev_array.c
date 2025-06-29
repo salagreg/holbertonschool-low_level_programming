32902729f3a212384873adeedd13c5df0390c189
root <greg.sala@icloud.com>
2025-06-29 12:19:51 +0000

    test

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* <nom de la fonction> - <explication de la fonction>
+* @<nom de l'argument>: <explication de l'argument> // Seulement s'il y a un ou plusieurs arguments
+* Return: <explication des valeurs de retour> // Seulement si la fonction retourne autre chose que void
+*/
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+
+	
