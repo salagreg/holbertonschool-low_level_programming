db9efa30b64a9964fcf700ebb24b2a568cc2f069
root <greg.sala@icloud.com>
2025-06-30 10:59:06 +0000

    remplir la mémoire

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _memset - remplir la mémoire avec un octet constant
+* @s: pointeur vers une zone mémoire
+* @b : caractère octet
+* @n : nombres d'octets à remplir
+* Return: s, pour la valeur pointée
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+/*boucle pour parcourir la mémoire*/
+	for (i = 0; i < n; i++)
+	{
+		/*remplir toutes les cases*/
+		s[i] = b;
+	}
+	return (s);
+}
