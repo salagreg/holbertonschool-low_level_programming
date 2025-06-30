136e13cfe892cec1aa9f051a92418842bcd0f0a9
root <greg.sala@icloud.com>
2025-06-30 11:51:33 +0000

    copier zone memoire

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _memcpy - copier une zone de mémoire
+* @dest : pointe sur le fichier copié
+* @src : pointe sur le fichier à copié
+* @n : nombres d'octets
+* Return: dest, le fichier copié pointé
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	int j = 0;
+	int temp;
+	/*boucle pour parcourir ma zone de mémoire*/
+	for (i = 0; i < n; i++)
+	{
+		/*formule pour copier un fichier dans un autre*/
+		src[i] = temp;
+		temp = dest[j];
+		src[i] = dest[j];
+	}
+	return (dest);
+}
