708b066c48bf8b852f6fdaffaafb8682426319d3
root <greg.sala@icloud.com>
2025-07-01 11:57:11 +0000

    task 7

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* <nom de la fonction> - <explication de la fonction>
+* @<nom de l'argument>: <explication de l'argument> // Seulement s'il y a un ou plusieurs arguments
+* Return: <explication des valeurs de retour> // Seulement si la fonction retourne autre chose que void
+*/
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int prem = 0;
+	int sec = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		
