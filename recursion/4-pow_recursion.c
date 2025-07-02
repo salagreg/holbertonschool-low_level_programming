f1f946b0ba5bd7e1bdc73764d028057bef3fcc08
root <greg.sala@icloud.com>
2025-07-02 15:55:10 +0000

    renvoyer valeur / puissance

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -3,18 +3,20 @@
 * _pow_recursion - fonction qui renvoie la valeur
 * @x : contient la valeur de x
 * @y : contient la puissance de y
-* Return: 0
+* Return: la récursion
 */
 int _pow_recursion(int x, int y)
 {
+	/*condition donnée pour test si erreur*/
 	if (y < 0)
 	{
 		return (-1);
 	}
+	/*condition de cas de base*/
 	if (y == 0)
 	{
 		return (1);
 	}
-	_pow_recursion(x);
-	return (0);
+	/*retourne la récursion: x * result precedent*/
+	return (x * _pow_recursion(x, y - 1));
 }
