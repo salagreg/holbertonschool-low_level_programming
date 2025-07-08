e5c838af3bdf8a031fbfd5f9b52f28ea0a436101
root <greg.sala@icloud.com>
2025-07-08 08:29:20 +0000

    concatener 2 strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,21 +1,23 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
 /**
+ * str_concat - Concatène deux chaînes de caractères
+ * @s1: Première chaîne de caractères à concaténer
+ * @s2: Deuxième chaîne de caractères à concaténer
  *
- *
- *
- *
+ * Return: NULL si l’allocation échoue, sinon un pointeur vers la
+ *         nouvelle chaîne contenant s1 suivie de s2, terminée par '\0'
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, k;
+	int i, j, k, m;
+	char *newstr;
 
 	if (s1 == NULL)
-		return("");
+		return (NULL);
 	/*redirection vers une chaine vide temporaire*/
 	if (s2 == NULL)
-		return ("");
+		return (NULL);
 	/*parcourir 1ere chaine*/
 	for (i = 0; s1[i] != '\0'; i++)
 		;
@@ -23,16 +25,19 @@ char *str_concat(char *s1, char *s2)
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 	/*allouer la memoire avec malloc*/
-	char *newstr = malloc((i + j + 1) * sizeof(char));
-        /*vérifier si malloc a échoué*/
+	newstr = malloc((i + j + 1) * sizeof(char));
+/*vérifier si malloc a échoué*/
 	if (newstr == NULL)
 		return (NULL);
-
+	/*boucle pour copier s1*/
 	for (k = 0; k < i; k++)
+	{
 		newstr[k] = s1[k];
-	for (int copy = 0; copy < j; copy++)
-		newstr[copy++] = s2[copy]
+	}
+	/*boucle pour aller de i à j-1*/
+	for (m = 0; m < j; m++)
+		newstr[k + m] = s2[m];
 
-	newstr[k] = '\0';
+	newstr[k + m] = '\0';
 	return (newstr);
 }
