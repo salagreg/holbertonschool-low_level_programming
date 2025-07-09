b77a17ba23c27b1323738493f8bdd36e76cc6144
root <greg.sala@icloud.com>
2025-07-09 08:11:36 +0000

    allouer memoire

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * malloc_checked - Alloue de la mémoire avec malloc
+ * @b: taille en octets à allouer
+ * Return: Un pointeur vers la mémoire allouée
+ *         Termine le programme avec exit 98 si malloc échoue
+ */
 void *malloc_checked(unsigned int b)
 {
 	/*prend la valeur en octet de ptr*/
