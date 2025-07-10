f9215b6f407abc6b1e96f069b3d8cb72158c3944
root <greg.sala@icloud.com>
2025-07-10 11:09:14 +0000

    définir struct

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * struct dog - Structure représentant un chien
+ * @name: Nom du chien
+ * @age: Âge du chien (en années)
+ * @owner: Nom du propriétaire du chien
+ *
+ * Description: Structure contenant les informations de base
+ * sur un chien, incluant son nom, son âge et son propriétaire.
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
