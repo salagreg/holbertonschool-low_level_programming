801558ccf25bba83e43ab32f392840ec6cc1ad62
root <greg.sala@icloud.com>
2025-07-11 15:44:40 +0000

    allocates memory

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	ptr = malloc((nmemb * size) *sizeof(int));
+	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 	{
 		return (NULL);
