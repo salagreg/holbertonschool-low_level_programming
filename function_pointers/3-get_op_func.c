348288e3ce31866d4cbd266d18cf03c1e1e7be49
root <greg.sala@icloud.com>
2025-07-17 13:50:22 +0000

    Performs simple operations

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,6 +1,17 @@
 #include "3-calc.h"
-#include <stdlib.h>
-
+#include <string.h>
+#include "3-calc.h"
+#include <string.h>
+/**
+ * get_op_func - Sélectionne la fonction correcte selon l’opérateur donné
+ * @s: L’opérateur en chaîne de caractères (ex: "+", "-", "*", etc.)
+ *
+ * Description: Parcourt un tableau de structures contenant des opérateurs
+ * et leurs fonctions associées, puis retourne un pointeur vers la fonction
+ * correspondante si elle est trouvée.
+ *
+ * Return: Pointeur vers la fonction correspondante, ou NULL si non trouvé
+ */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -17,9 +28,8 @@ int (*get_op_func(char *s))(int, int)
 	{
 		if (strcmp(ops[i].op, s) == 0)
 		{
-			return (ops[i].op);
-			i++;
+			return (ops[i].f);
 		}
-		return (NULL);
 	}
+	return (NULL);
 }
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,15 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
-/*
- *Doit contenir que le fichier principal
- *Utiliser atoi pour convertir argument en int
- *Pas de boucles
- *Max 3 conditions if
+/**
+ * main - Point d'entrée du programme de calcul
+ * @argc: Nombre d'arguments passés au programme
+ * @argv: Tableau de chaînes représentant les arguments
+ *
+ * Description: Ce programme effectue une opération mathématique
+ * entre deux entiers passés en argument. Il utilise une fonction
+ * correspondant à l'opérateur fourni.
+ *
+ * Return: 0 si succès, ou un code d'erreur spécifique (98, 99, 100)
  */
-int main(void)
+int main(int argc, char *argv[])
 {
+	int argument1, argument2, result;
+	char c;
+	int (*function)(int, int);
 
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	argument1 = atoi(argv[1]);
+	argument2 = atoi(argv[3]);
 
+	function = get_op_func(argv[2]);
 
+	if (!function)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
+	c = *argv[2];
+
+	if ((c == '/' || c == '%') && argument2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = function(argument1, argument2);
+	printf("%d\n", result);
+	return (0);
 }
