4c0934cede42a6b98eb9a49ced18f646fa03ee36
root <greg.sala@icloud.com>
2025-07-17 22:59:34 +0000

    prints strings

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -24,13 +24,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		string = va_arg(arg, char *);
 
+		if (separator == NULL)
+			return;
 		if (string == NULL)
 			printf("(nil)\n");
 
 		printf("%s", string);
-
-		if (separator == NULL)
-                        return;
 
 		if (i < n - 1 && separator != NULL)
 			printf("%s", separator);
