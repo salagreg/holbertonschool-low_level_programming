cbaec0190123b765f656bf5687e62ad76d4588b6
root <greg.sala@icloud.com>
2025-07-29 12:48:29 +0000

    returns the number of elements

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,18 +1,18 @@
 #include "lists.h"
-/*
+/**
   * list_len - returns the number of elements in a linked list
   * @h: pointer to the head of the list
   *
   * Return: number of elements in the list
-  */  
+  */
 size_t list_len(const list_t *h)
 {
-  size_t nbr_nodes = 0;
+	size_t nbr_nodes = 0;
 
-  while (h != NULL)
-  {
-    h = h->next;
-    nbr_nodes++;
-  }
-  return (nbr_nodes);
+	while (h != NULL)
+	{
+		h = h->next;
+		nbr_nodes++;
+	}
+	return (nbr_nodes);
 }
