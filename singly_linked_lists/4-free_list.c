8c56f8c18eb82bd0c734439d553a9071cd163267
root <greg.sala@icloud.com>
2025-07-29 12:53:53 +0000

    that frees a list_t list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -9,13 +9,13 @@
  */
 void free_list(list_t *head)
 {
-  list_t *temp = NULL;
+	list_t *temp = NULL;
 
-  while (head != NULL)
-  {
-    temp = head;
-    head = head->next;
-    free(temp->str);
-    free(temp);
-  }
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
 }
