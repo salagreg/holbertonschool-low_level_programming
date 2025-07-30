f021981f6462c0961d0d91d676e6d39a13e82c60
salagreg <greg.sala@icloud.com>
2025-07-30 14:34:06 +0200

    returns the number of elements

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * dlistint_len - returns the number of elements in a dlistint_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of elements in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+  size_t nbr_elements = 0;
+
+  while (h != NULL)
+  {
+    nbr_elements++;
+    h = h->next;
+  }
+  return (nbr_elements);
+}
