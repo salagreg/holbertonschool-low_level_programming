c334ac07510e526a0303c8c57d402816726d1af3
salagreg <greg.sala@icloud.com>
2025-07-30 23:57:27 +0200

    frees a dlistint_t list

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
-
+/**
+ * free_dlistint - Frees a doubly linked list.
+ * @head: Pointer to the head of the doubly linked list.
+ *
+ * Description: This function iterates through a doubly linked list,
+ * freeing the memory allocated for each node.
+ */
 void free_dlistint(dlistint_t *head)
 {
   dlistint_t *temp;
