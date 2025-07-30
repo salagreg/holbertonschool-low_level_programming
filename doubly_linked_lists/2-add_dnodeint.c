ef40761274062e4f4d6c6d1162e637620120780b
root <greg.sala@icloud.com>
2025-07-30 13:33:28 +0000

    adds a new node at the beginning

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -8,21 +8,21 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-  dlistint_t *new_node;
+	dlistint_t *new_node;
 
-  new_node = malloc(sizeof(dlistint_t));
-  if (new_node == NULL)
-    return (NULL);
-    
-  new_node->n = n;
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-  new_node->next = *head;
-  new_node->prev = NULL;
-  
-  if (*head != NULL)
-    (*head)->prev = new_node;
+	new_node->n = n;
 
-  *head = new_node;
+	new_node->next = *head;
+	new_node->prev = NULL;
 
-  return (new_node);
+	if (*head != NULL)
+		(*head)->prev = new_node;
+
+	*head = new_node;
+
+	return (new_node);
 }
