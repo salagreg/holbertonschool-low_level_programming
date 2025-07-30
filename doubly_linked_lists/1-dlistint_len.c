53ac4fd55e70bb346391e9527179298f209f2df0
root <greg.sala@icloud.com>
2025-07-30 13:19:59 +0000

    returns the number of elements

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -7,12 +7,12 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-  size_t nbr_elements = 0;
+	size_t nbr_elements = 0;
 
-  while (h != NULL)
-  {
-    nbr_elements++;
-    h = h->next;
-  }
-  return (nbr_elements);
+	while (h != NULL)
+	{
+		nbr_elements++;
+		h = h->next;
+	}
+	return (nbr_elements);
 }
