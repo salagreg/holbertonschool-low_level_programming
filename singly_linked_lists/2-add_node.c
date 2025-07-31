d682ad95a675d7919f42e6c27c8d41fbcb0b0339
salagreg <greg.sala@icloud.com>
2025-07-31 15:27:34 +0200

    adds a new node at the beginning

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,6 +11,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	int i = 0;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
@@ -25,7 +26,11 @@ list_t *add_node(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
-	new_node->len = strlen(str);
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	new_node->len = i;
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
